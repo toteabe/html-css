55c7b12315b86ad390fb7ee1cf14d1103b19bdc2
prof <56897045+toteabe@users.noreply.github.com>
2019-11-08 11:27:28 +0100

    remote

--- remote-git-hub.bat
+++ remote-git-hub.bat
@@ -1,2 +1,3 @@
-git remote add origin https://github.com/toteabe/htm-css.git
+#Establece conexión con el repositorio remoto, referido por defecto como origin
+git remote add origin https://github.com/<usuario>/<repo>.git
 git remote -v
